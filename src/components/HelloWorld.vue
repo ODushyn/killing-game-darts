6cd093c6aa12ee59c82947141cea0dd9aac7032b
odushyn <oleksandr.dushyn@sportradar.com>
2020-02-21 21:14:28 +0100

    add game over and recoveries

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,15 +12,23 @@
       <tr v-for="player in players" :key="player.name"
           v-bind:class="{
             'current-player': isCurrentPlayer(player),
-            'dead-player': isDeadPlayer(player)
+            'dead-player': isPlayerRIP(player)
           }"
       >
         <td>{{player.name}}</td>
         <td>{{player.num}}</td>
         <td v-for="(_throw, index) in player.throws" :key="index">
-          <input disabled v-model="_throw.value" v-if="!isDeadPlayer(player)"/>
+          <input disabled v-model="_throw.value"
+                 v-if="!isPlayerRIP(player)"
+                 v-bind:class="{'current-throw': isCurrentThrow(player, index)}"/>
         </td>
-        <td> {{player.lives}}</td>
+        <td>
+          {{player.lives}}
+          <img src="@/assets//heart.png"
+               v-if="player.recoveries === 1"
+               alt="heart"
+               style="width:10px; height:auto;">
+        </td>
       </tr>
     </table>
   </div>
@@ -40,8 +48,8 @@
     data() {
       return {
         players: [
-          {name: 'Felipe', num: '10', throws: [{value: ''}, {value: ''}, {value: ''}], was_dead: false, lives: 3},
-          {name: 'Sasha', num: '20', throws: [{value: ''}, {value: ''}, {value: ''}], was_dead: false, lives: 3}
+          {name: 'Felipe', num: '10', throws: [{value: ''}, {value: ''}, {value: ''}], recoveries: 1, lives: 3},
+          {name: 'Sasha', num: '20', throws: [{value: ''}, {value: ''}, {value: ''}], recoveries: 1, lives: 3}
         ]
       }
     },
@@ -53,8 +61,11 @@
       isCurrentPlayer: (player) => {
         return currentPlayer().num === player.num;
       },
-      isDeadPlayer: (player) => {
-        return isPlayerDead(player);
+      isPlayerRIP: (player) => {
+        return isPlayerRIP(player);
+      },
+      isCurrentThrow: (player, index) => {
+        return player.num === currentPlayer().num && currentThrow === index + 1;
       }
     },
     created() {
@@ -81,18 +92,27 @@
             } else if (isNextPlayer()) {
               startNewPlayer();
             } else {
+              finishCurrentRound();
               startNewRound();
             }
+            self.$forceUpdate();
           }
         }
       });
     }
   }
 
-  //TODO: move all function to methods
+  //TODO: move all function to methods?
   function isGameOver() {
     // game is over if there only one players having lives
     // all other players must have 0 lives and was_dead = true
+    let alivePlayers = players.filter(player => !isPlayerRIP(player));
+    if(alivePlayers.length === 1) {
+      alert(`${alivePlayers[0].name} ----- Congrats!`);
+      return true;
+    }
+
+    return false;
   }
 
   function startNewRound() {
@@ -101,27 +121,33 @@
     players.forEach((player) => player.throws = [{value: ''}, {value: ''}, {value: ''}]);
   }
 
+  function finishCurrentRound() {
+    if(currentPlayer().lives === 0) {
+      currentPlayer().recoveries--;
+    }
+    if(isPlayerRIP(currentPlayer())){
+      alert(`${currentPlayer().name} ----- RIP`)
+    }
+  }
+
   function startNextThrow() {
     currentThrow++;
   }
 
-  // function isNextThrow() {
-  //   const THROWS_NUM = 3;
-  //   return currentThrow < THROWS_NUM;
-  // }
-
   function finishCurrentThrow() {
     let damagedPlayer = players.find(player => player.num === _getKilledPlayer());
-    if (!damagedPlayer) {
-      return;
+
+    if (damagedPlayer && !isPlayerRIP(damagedPlayer)) {
+      if(currentPlayer().num !== damagedPlayer.num) {
+        if(damagedPlayer.lives > 0) damagedPlayer.lives--;
+        if(damagedPlayer.lives === 0) damagedPlayer.recoveries--;
+      } else {
+        if(currentPlayer().lives < 3) currentPlayer().lives++;
+      }
     }
 
-    //TODO: consider if player has chance to recover
-    if (!isPlayerDead(currentPlayer())) {
-      damagedPlayer.lives--;
-      if (damagedPlayer.lives === 0) {
-        alert(`${damagedPlayer.name} ----- RIP`)
-      }
+    if (damagedPlayer && isPlayerRIP(damagedPlayer)) {
+      alert(`${damagedPlayer.name} ----- RIP`)
     }
 
     function _getKilledPlayer() {
@@ -136,8 +162,8 @@
     currentThrow = 1;
   }
 
-  function isPlayerDead(player) {
-    return player.lives === 0;
+  function isPlayerRIP(player) {
+    return player.lives === 0 && player.recoveries === -1;
   }
 
   function currentPlayer() {
@@ -191,7 +217,7 @@
   }
 
   .current-throw {
-    color: #42b983;
+    background-color: cornflowerblue;
   }
 
   .current-player {
