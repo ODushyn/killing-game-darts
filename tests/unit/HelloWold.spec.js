8c44b5265bacf2fef17853ef49f0cd346108ce15
odushyn <oleksandr.dushyn@sportradar.com>
2020-02-22 17:49:17 +0100

    rename to game component

--- tests/unit/HelloWold.spec.js
+++ tests/unit/HelloWold.spec.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/Game.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Game.vue', () => {
   it('renders props.msg when passed', () => {
     const players = [];
     const wrapper = shallowMount(HelloWorld, {
